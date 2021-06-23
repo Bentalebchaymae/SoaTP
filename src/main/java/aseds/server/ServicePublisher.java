afd3097739ad04cb0019a4efb24fd6b3ab9d8631
Bentalebchaymae <chaymaebentalen98@gmail.com>
2021-06-23 12:39:02 -0400

    commit

---
+++ src/main/java/aseds/server/ServicePublisher.java
@@ -0,0 +1,17 @@
+package aseds.server;
+import javax.xml.ws.Endpoint;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import aseds.webservices.ProductsManagerImpl;
+public class ServicePublisher {
+    private static Logger logger = (Logger) LoggerFactory.getLogger(ServicePublisher.class);
+    public static void main(String[] args) {
+
+        logger.info("Starting service");
+        Endpoint.publish("http://localhost:2021/webservices/PoductsMAnager", new ProductsManagerImpl());
+        logger.info("Server started");
+    }
+
+}
---
+++ src/main/java/aseds/webservices/ProductsManagerImpl.java
@@ -0,0 +1,54 @@
+package aseds.webservices;
+import java.util.ArrayList;
+
+import javax.jws.WebService;
+
+import aseds.domain.Product;
+import aseds.domain.Products;
+import aseds.exceptions.NoSuchProductException;
+@WebService(endpointInterface = "aseds.webservices.ProductsManager")
+public class ProductsManagerImpl implements  ProductsManager {
+    private static Products products = new Products(new ArrayList<Product>());
+
+    public long addProduct(Product product) {
+        return products.add(product);
+    }
+
+    public Product getProduct(long id) throws NoSuchProductException {
+        Product product = products.get(id);
+
+        if(product != null) {
+            return product;
+        } else {
+            throw new NoSuchProductException("No such product");
+        }
+    }
+
+    public double getProductPrice(long id) throws NoSuchProductException {
+        Product product = products.get(id);
+        if(product != null) {
+            return product.getPrice();
+        } else {
+            throw new NoSuchProductException("No such product");
+        }
+    }
+
+    public Products getProducts() {
+        return products;
+    }
+
+    public Product updateProduct(Product product) {
+
+        return null;
+    }
+
+    public boolean deleteProduct(long id) {
+
+        return products.remove(id);
+    }
+
+    public boolean deleteAllProducts() {
+
+        return products.removeAll();
+    }
+}
